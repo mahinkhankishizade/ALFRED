40ed28631be42e97834e479b271cb63e41bd4265
Mahin Khankishizade <32219005+mahinkhankishizade@users.noreply.github.com>
2018-03-08 00:22:18 +0300

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -4,9 +4,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
@@ -19,63 +17,76 @@ public class Parser {
     ArrayList<String> hints;
     int[] colorsOfBlocks;
     int[] numbersOfBlocks;
+    char[] lettersOfBlocks;
+
     String html;
     String textFile = "7MarchPuzzleCode";
 
+    private String solutionAddress;
 
-    public Parser() {
+
+    public Parser() throws FileNotFoundException {
         // initialization
         hints = new ArrayList<String>();
         colorsOfBlocks = new int[25];
         numbersOfBlocks = new int[25];
+        lettersOfBlocks = new char[25];
 
-        html = fetch( "https://www.nytimes.com/crosswords/game/mini");
+        //html = fetch("https://www.nytimes.com/crosswords/game/mini");
 
-        // convertToString(); // use for old puzzles
+        convertToString(); // use for old puzzles
     }
 
     // getters and setters
-    public void setHTMLString( String html) {
+    public void setHTMLString(String html) {
         this.html = html;
     }
     public String getHTMLString() {
         return html;
     }
-    public int[] getNumbersOfBlocks() { return numbersOfBlocks; }
-    public int[] getColorsOfBlocks() { return colorsOfBlocks; }
-    public ArrayList<String> getAllHints() { return hints; }
+    public int[] getNumbersOfBlocks() {
+        return numbersOfBlocks;
+    }
+    public int[] getColorsOfBlocks() {
+        return colorsOfBlocks;
+    }
+    public ArrayList<String> getAllHints() {
+        return hints;
+    }
+    public char[] getLettersOfBlocks() { return lettersOfBlocks; }
 
     // for using old puzzles
     public void convertToString() throws FileNotFoundException {
-        String entireFileText = new Scanner( new File( textFile + ".txt")).useDelimiter("\\A").next();
-        setHTMLString( entireFileText);
-        getHints( entireFileText);
-        getColors( entireFileText);
-        getNumbers( entireFileText);
+        String entireFileText = new Scanner(new File(textFile + ".txt")).useDelimiter("\\A").next();
+        setHTMLString(entireFileText);
+        getHints(entireFileText);
+        getColors(entireFileText);
+        getNumbers(entireFileText);
+        getLetters(entireFileText);
     }
 
-
     // this method fetchs the html code from the website and writes it to the textfile
-    public String fetch( String html) {
+    public String fetch(String html) {
         try {
-            Document doc = Jsoup.connect( html).get();
+            Document doc = Jsoup.connect(html).get();
             String htmlDocument = doc.toString();
-            Elements links = doc.select( "link");
-            Elements scripts = doc.select( "script");
-            for( Element element : links) {
-                htmlDocument += element.absUrl( "href");
+            Elements links = doc.select("link");
+            Elements scripts = doc.select("script");
+            for (Element element : links) {
+                htmlDocument += element.absUrl("href");
             }
-            for( Element element : scripts) {
+            for (Element element : scripts) {
                 htmlDocument += element.absUrl("src");
             }
 
             // save html code in the text file
             String path = "C:/Users/User/Desktop/School/cs461/Project/" + textFile + ".txt";
-            Files.write( Paths.get(path), htmlDocument.getBytes(), StandardOpenOption.CREATE);
+            Files.write(Paths.get(path), htmlDocument.getBytes(), StandardOpenOption.CREATE);
 
-            getHints( htmlDocument);
-            getColors( htmlDocument);
-            getNumbers( htmlDocument);
+            getHints(htmlDocument);
+            getColors(htmlDocument);
+            getNumbers(htmlDocument);
+            getLetters(htmlDocument);
 
             return htmlDocument;
 
@@ -85,35 +96,36 @@ public class Parser {
         }
     }
 
-    public void getHints( String html) {
-        Document document = Jsoup.parse( html);
+    public void getHints(String html) {
+        Document document = Jsoup.parse(html);
         Elements numbers = document.getElementsByClass("Clue-label--2IdMY");
         Elements clues = document.getElementsByClass("Clue-text--3lzl7");
 
-        for( int i = 0; i < numbers.size() && i < clues.size(); i++) {
-            hints.add( numbers.get(i).text() + ". " + clues.get(i).text());
-            System.out.println( numbers.get(i).text() + ". " + clues.get(i).text());
+        for (int i = 0; i < numbers.size() && i < clues.size(); i++) {
+            hints.add(numbers.get(i).text() + ". " + clues.get(i).text());
+            System.out.println(numbers.get(i).text() + ". " + clues.get(i).text());
         }
     }
 
     // extract the grid part from the text file
-    public String cutHTML( String html) {
+    public String cutHTML(String html) {
         String startIndex = "<g data-group=\"cells\"";
         String endIndex = "<g data-group=\"grid\"";
 
-        int start = html.indexOf( startIndex) + startIndex.length();
-        int end = html.indexOf( endIndex) - 1;
+        int start = html.indexOf(startIndex) + startIndex.length();
+        int end = html.indexOf(endIndex) - 1;
 
-        return html.substring( start, end);
+        return html.substring(start, end);
     }
+
     // i need to get colors for blocks
-    public void getColors( String html) {
+    public void getColors(String html) {
         int size = 25;
 
         // cutting html only cell parts left
-        String newHTML = cutHTML( html);
+        String newHTML = cutHTML(html);
 
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             int indexStart = newHTML.indexOf("class=\"") + 7;
             int indexEnd = indexStart + 16;
             String color = newHTML.substring(indexStart, indexEnd);
@@ -125,44 +137,59 @@ public class Parser {
 
             newHTML = newHTML.substring(indexEnd);
         }
-        for( int i = 0; i < 25; i++)
+        for (int i = 0; i < 25; i++)
             System.out.print(colorsOfBlocks[i] + " ");
         System.out.println("end");
     }
 
     // i need to get numbers for blocks
-    public void getNumbers( String html) {
+    public void getNumbers(String html) {
         int size = 25;
 
-        String newHTML = cutHTML( html);
-        Document document = Jsoup.parse( newHTML);
+        String newHTML = cutHTML(html);
+        Document document = Jsoup.parse(newHTML);
         Elements elements = document.getElementsByTag("g");
 
-        for( int i = 0; i < elements.size() && i < size; i++) {
+        for (int i = 0; i < elements.size() && i < size; i++) {
             String text = elements.get(i).text();
-            if( !text.isEmpty()) {
-                numbersOfBlocks[i] = Integer.parseInt( text);
-            }
-            else
+            if (!text.isEmpty()) {
+                text = text.substring(0, 1);
+                if( text.matches("-?\\d+"))
+                    numbersOfBlocks[i] = Integer.parseInt(text);
+                else
+                    numbersOfBlocks[i] = 0;
+            } else
                 numbersOfBlocks[i] = 0;
         }
-        for( int i = 0; i < 25; i++)
+        for (int i = 0; i < 25; i++)
             System.out.print(numbersOfBlocks[i] + " ");
+        System.out.println("end");
     }
 
-    // i need to get the blocked grid texts
-    public void extractLetters( String html) throws IOException {
-        ArrayList<String> letters = new ArrayList<>();
+    public void getLetters( String html) {
+        int size = 25;
+        char[] temp = new char[25];
+        int j = 0;
 
-        String url = "https://www.nytimes.com/crosswords/game/mini";
-        Document document = Jsoup.connect(url).get();
-        String htmlCode = document.html();
-        Document doc = Jsoup.parse( htmlCode);
-        Elements elements = doc.getElementsByTag("g");
-        for( int i = 0; i < elements.size(); i++) {
-            if( elements.get(i).hasClass("Cell-cell--1p4gH")) {
+        String newHTML = cutHTML(html);
 
+        for (int i = 0; i < size; i++) {
+            int indexStart = newHTML.indexOf("66.67\"") + 7;
+            int indexEnd = indexStart + 1;
+            String letter = newHTML.substring(indexStart, indexEnd);
+            temp[i] = letter.charAt(0);
+            newHTML = newHTML.substring(indexEnd);
+        }
+        for( int i = 0; i < size; i++) {
+            if( colorsOfBlocks[i] == 1)
+                lettersOfBlocks[i] = ' ';
+            else {
+                lettersOfBlocks[i] = temp[j];
+                j++;
             }
         }
+        for (int i = 0; i < 25; i++)
+            System.out.print(lettersOfBlocks[i] + " ");
+        System.out.println("end");
     }
 }
